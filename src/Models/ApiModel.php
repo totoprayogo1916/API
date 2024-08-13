9b64c705eea306a9c231838761b513b6f6e1466d
totoprayogo1916 <2387514+totoprayogo1916@users.noreply.github.com>
2024-08-13 15:47:46 +0700

    add model

---
+++ src/Models/ApiModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Esoftdream\Response;
+
+use CodeIgniter\Database\BaseBuilder;
+use CodeIgniter\Model;
+
+class ApiModel extends Model
+{
+    protected $table = 'log_response_api';
+}
---
+++ src/Models/CallbackModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Esoftdream\Response;
+
+use CodeIgniter\Database\BaseBuilder;
+use CodeIgniter\Model;
+
+class CallbackModel extends Model
+{
+    protected $table = 'log_response_callback';
+}
